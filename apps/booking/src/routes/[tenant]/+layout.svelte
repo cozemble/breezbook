bd20cf7dfd40ec43f96fa567511acc75d4467c38
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-12-10 17:04:07 +0300

    feat(booking): layout, header, footer

---
+++ apps/booking/src/routes/[tenant]/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import Footer from './Footer.svelte';
+	import Header from './Header.svelte';
+
+	export let data: PageData;
+	const tenant = data.tenant;
+</script>
+
+<div class="min-h-screen flex flex-col">
+	<Header {tenant} />
+	<main class="p-6 flex-grow">
+		<slot />
+	</main>
+	<Footer {tenant} />
+</div>
---
+++ apps/booking/src/routes/[tenant]/Footer.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let tenant: Tenant;
+</script>
+
+<footer class="footer px-6 py-4 justify-between bg-neutral text-neutral-content">
+	<p class="text-center">{tenant.name}</p>
+	<p class="text-center text-xs opacity-50">
+		Provided by breezbook &copy; {new Date().getFullYear()}
+	</p>
+</footer>
