19e9826010883c02dd15534334ecdf235df045bf
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-12-14 12:43:29 +0300

    feat(booking): booking page breadcrumbs and steps

--- apps/booking/src/routes/[tenant]/[service]/booking/+page.svelte
+++ apps/booking/src/routes/[tenant]/[service]/booking/+page.svelte
@@ -7,8 +7,28 @@
 	const service = data.service;
 </script>
 
+<div class="text-sm breadcrumbs">
+	<ul>
+		<li><a href="/{tenant.slug}">Services</a></li>
+		<li><a href="/{tenant.slug}/{service.slug}">{service.name}</a></li>
+		<li>Booking</li>
+	</ul>
+</div>
 
 <!-- Booking area -->
+<section class="">
+	<section class="flex flex-col justify-center items-center mb-10">
+		<!-- Steps -->
+		<div class=" flex-grow flex flex-col items-center">
+			<ul class="steps w-96">
+				<li class="step step-primary font-semibold text-primary text-lg">Pick a Time</li>
+				<li class="step">Extras</li>
+				<li class="step">Details</li>
+			</ul>
+		</div>
+
+	</section>
+
 	<section>
 		<div class="border-base-300">
 			<!-- <h1 class="text-xl text-secondary font-semibold pb-2 mb-6 border-b">Pick a time</h1> -->
@@ -20,3 +40,4 @@
 			<button class="btn btn-primary"> Next </button>
 		</div>
 	</section>
+</section>
