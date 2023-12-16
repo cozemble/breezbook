0fd55b9227ae44f9ce67d051a7bce40d1a26db83
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-12-16 18:44:47 +0300

    feat(booking): service hero better styling

--- apps/booking/src/routes/[tenant]/[service]/+page.svelte
+++ apps/booking/src/routes/[tenant]/[service]/+page.svelte
@@ -10,29 +10,39 @@
 	<title>{tenant.name} - {service.name}</title>
 </svelte:head>
 
-<!-- Service hero -->
-<section class="flex justify-between items-center my-10 rounded-lg">
-	<div>
-		<h1 class="text-2xl text-primary font-semibold">{service.name}</h1>
-		<p class="text-lg">{service.description}</p>
+<section class="hero flex justify-between items-center my-10 rounded-lg">
+	<div
+		class="hero-content w-full px-0 justify-between
+		flex-col-reverse gap-10 items-start sm:flex-row sm:items-center"
+	>
+		<div class="">
+			<h1 class="text-3xl capitalize text-primary font-bold">{service.name}</h1>
+			<p class="text-base">{service.description}</p>
 
-		<!-- TODO format properly -->
-		<div class="text-2xl font-bold mt-4">
-			<span class="opacity-30 text-2xl"> ≈ </span>
-
-			£{service.approximatePrice}
-			<span class="text-base font-thin opacity-60 whitespace-nowrap"
-				>/ {service.approximateDuration} mins</span
-			>
+			<!-- pricing -->
+			<div class="mt-4">
+				<span class="text-xs font-light opacity-60"> starting at: </span>
+				<div class="text-2xl font-bold">
+					<span class="text-4xl font-bold">
+						£{service.approximatePrice}
+						<span class="text-base font-thin opacity-60 whitespace-nowrap"
+							>/
+							{service.approximateDuration} mins
+						</span>
+					</span>
+				</div>
+			</div>
+			<a href="./{service.slug}/booking" class="btn btn-primary mt-8">Book Now</a>
 		</div>
-		<a href="./{service.slug}/booking" class="btn btn-primary mt-8">Book Now</a>
-	</div>
 
-	<img
-		src={service.image}
-		alt="service banner"
-		class="rounded-lg shadow-2xl aspect-[5/2] h-60 object-cover"
-	/>
+		<img
+			src={service.image}
+			alt="service banner"
+			class="rounded-lg shadow-lg aspect-video
+			min-w-0 w-full sm:max-w-sm"
+			aria-hidden
+		/>
+	</div>
 </section>
 
 <!-- TODO details of the service -->
