2b4cc2981c02d7c3e9d8e5f459b221c86a9b3f48
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-12-16 18:31:20 +0300

    chore(booking): footer link for breezbook for easy navigation

--- apps/booking/src/routes/[tenant]/Footer.svelte
+++ apps/booking/src/routes/[tenant]/Footer.svelte
@@ -4,7 +4,9 @@
 
 <footer class="footer px-6 py-4 justify-between bg-neutral text-neutral-content">
 	<p class="text-center">{tenant.name}</p>
-	<p class="text-center text-xs opacity-50">
-		Provided by breezbook &copy; {new Date().getFullYear()}
+	<p class="text-center text-xs opacity-50 inline">
+		Provided by
+		<a href="/" class="link-hover"> breezbook </a>
+		&copy; {new Date().getFullYear()}
 	</p>
 </footer>
