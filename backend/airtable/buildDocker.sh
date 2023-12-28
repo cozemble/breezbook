2f816caa163a9de6b4adad073a60e17dae48873c
mikehogan <me@mikehogan.net>
2023-12-28 14:43:18 +0000

    edit docker build process now that prisma generation is required

--- backend/airtable/buildDocker.sh
+++ backend/airtable/buildDocker.sh
@@ -3,5 +3,6 @@
 ../scripts/packaging/rewrite-workspace-deps.sh
 cp Dockerfile .dockerignore staging
 cp tsconfig.json staging
+cp -r prisma staging
 docker build --platform linux/amd64 -t airtable-api .
 docker tag airtable-api europe-west2-docker.pkg.dev/cozemble/main-docker-repo/breezbook/backend-airtable:latest
