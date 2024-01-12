099b0155f08e616180603e3f266f399bd71b7e8a
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-01-12 20:00:51 +0300

    style(booking): @typescript-eslint/no-explicit-any: warn

--- apps/booking/.eslintrc.cjs
+++ apps/booking/.eslintrc.cjs
@@ -29,6 +29,7 @@ module.exports = {
 		}
 	],
 	rules: {
-		'@typescript-eslint/no-unused-vars': 'warn'
+		'@typescript-eslint/no-unused-vars': 'warn',
+		'@typescript-eslint/no-explicit-any': 'warn'
 	}
 };
