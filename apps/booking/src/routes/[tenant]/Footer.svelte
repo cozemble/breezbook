f2f1df68aa3652c188ff5d88f24fb3ac4af236c1
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-01-20 19:29:52 +0300

    feat(breezbook): header and footer max width

--- apps/booking/src/routes/[tenant]/Footer.svelte
+++ apps/booking/src/routes/[tenant]/Footer.svelte
@@ -4,11 +4,13 @@
 	const tenant = tenantStore.get();
 </script>
 
-<footer class="footer px-3 md:px-6 py-4 justify-between bg-neutral text-neutral-content">
-	<p class="text-center">{tenant.name}</p>
-	<p class="text-center text-xs opacity-50 inline">
-		Provided by
-		<a href="/" class="link-hover"> breezbook </a>
-		&copy; {new Date().getFullYear()}
-	</p>
+<footer class="footer px-3 md:px-6 py-4 flex justify-center bg-neutral text-neutral-content">
+	<div class="w-full max-w-7xl flex justify-between">
+		<p class="text-center">{tenant.name}</p>
+		<p class="text-center text-xs opacity-50 inline">
+			Provided by
+			<a href="/" class="link-hover"> breezbook </a>
+			&copy; {new Date().getFullYear()}
+		</p>
+	</div>
 </footer>
--- apps/booking/src/routes/[tenant]/Header.svelte
+++ apps/booking/src/routes/[tenant]/Header.svelte
@@ -5,10 +5,17 @@
 	const tenant = tenantStore.get();
 </script>
 
-<header class="navbar px-3 md:px-6 flex border-b sticky top-0 left-0 right-0 z-50">
-	<div class="flex-1">
-		<a href="/{tenant.slug}" class="text-xl font-bold hover-link">{tenant.name}</a>
-	</div>
+<header
+	class="navbar px-3 md:px-6 py-4 flex justify-center
+	sticky top-0 left-0 right-0 z-50
+	border-b
+	"
+>
+	<div class="w-full max-w-7xl flex justify-between">
+		<div class="flex-1">
+			<a href="/{tenant.slug}" class="text-xl font-bold hover-link">{tenant.name}</a>
+		</div>
 
-	<CartNavButton />
+		<CartNavButton />
+	</div>
 </header>
