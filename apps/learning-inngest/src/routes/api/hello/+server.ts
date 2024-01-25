18b3c9a25ed01ac0b7fe74e9a37a0f803f8ead1f
mikehogan <me@mikehogan.net>
2024-01-25 09:07:05 +0000

    testing whether edge functions on vercel can use monorepo packages

--- apps/learning-inngest/src/routes/api/hello/+server.ts
+++ apps/learning-inngest/src/routes/api/hello/+server.ts
@@ -1,10 +1,14 @@
 import { inngest } from '$lib/inngest/client';
+import { type RequestEvent } from '@sveltejs/kit';
+import { mandatory } from '@breezbook/packages-core';
 
-export async function GET() {
+export async function GET(event: RequestEvent) {
+	const email = mandatory(event.url.searchParams.get('email'), `email`);
+
 	await inngest.send({
 		name: 'test/hello.world',
 		data: {
-			email: 'testFroSveltekit@example.com'
+			email
 		}
 	});
 	return new Response('OK', { status: 200 });
