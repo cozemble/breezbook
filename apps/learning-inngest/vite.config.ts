8215fefcc10cfd9afaf0d502d049377dd7a228bc
mikehogan <me@mikehogan.net>
2024-01-25 09:27:41 +0000

    does vercel plugin fix the issue?

--- apps/learning-inngest/vite.config.ts
+++ apps/learning-inngest/vite.config.ts
@@ -1,6 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), vercel()],
+	vercel: {
+		// optional configuration options, see "Advanced usage" below for details
+	}
 });
