b2ead6c5ed983ef3341655c56cbed81160645bfd
mikehogan <me@mikehogan.net>
2024-01-25 09:55:15 +0000

    remove the debug code

--- apps/learning-inngest/src/routes/+page.svelte
+++ apps/learning-inngest/src/routes/+page.svelte
@@ -2,14 +2,6 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
-	import { onMount } from 'svelte';
-	import { mandatory } from '@breezbook/packages-core';
-
-	onMount(() => {
-		const x = mandatory(new Date(), `Date is not defined`);
-		console.log({x})
-		console.log('onMount5');
-	});
 </script>
 
 <svelte:head>
@@ -37,31 +29,31 @@
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+    section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex: 0.6;
+    }
 
-	h1 {
-		width: 100%;
-	}
+    h1 {
+        width: 100%;
+    }
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
+    .welcome {
+        display: block;
+        position: relative;
+        width: 100%;
+        height: 0;
+        padding: 0 0 calc(100% * 495 / 2048) 0;
+    }
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+    .welcome img {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        display: block;
+    }
 </style>
