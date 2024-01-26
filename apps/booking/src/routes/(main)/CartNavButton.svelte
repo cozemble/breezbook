604cbf8c111053f02bd8efa08226ec6b3024c865
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-01-26 14:18:18 +0300

    refactor(booking): cart nav button should use a

--- apps/booking/src/routes/(main)/CartNavButton.svelte
+++ apps/booking/src/routes/(main)/CartNavButton.svelte
@@ -1,22 +1,14 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
-	import { goto } from '$app/navigation';
 	import { getCartStore } from '$lib/stores/cart';
-	import { tenantStore } from '$lib/stores/tenant';
 
 	const { items } = getCartStore();
-	const tenant = tenantStore.get();
 
 	$: itemCount = $items.length;
 	$: highlighted = itemCount > 0;
-
-	// <!-- TODO refactor this to a universal function -->
-	const goToCart = () => {
-		goto(`/checkout`);
-	};
 </script>
 
-<button class="btn {highlighted && 'btn-accent'}" on:click={goToCart}>
+<a href="/checkout" class="btn {highlighted && 'btn-accent'}">
 	<span class="indicator">
 		<!-- <Icon icon="ph:basket" class="w-6 h-6" /> -->
 		<Icon icon="solar:cart-linear" class="w-6 h-6" />
@@ -25,4 +17,4 @@
 	</span>
 
 	<span class="ml-2 hidden sm:inline"> Checkout </span>
-</button>
+</a>
