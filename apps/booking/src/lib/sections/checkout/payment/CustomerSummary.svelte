24c0a150de0e5d256dda77c55762cf2894eeed93
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-02-18 19:21:33 +0300

    feat(booking): payment page customer details summary

---
+++ apps/booking/src/lib/sections/checkout/payment/CustomerSummary.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import checkoutStore from '$lib/stores/checkout';
+
+	const {
+		customerStore: { customer }
+	} = checkoutStore.get();
+</script>
+
+<div class="card bg-base-200">
+	<div class="card-body">
+		<h1 class="text-xl font-bold">Your Details</h1>
+
+		<div class="flex gap-6">
+			<div>
+				<div class="font-semibold">Name</div>
+				<p class="inline">{$customer.firstName} {$customer.lastName}</p>
+			</div>
+
+			<div>
+				<div class="font-semibold">Email</div>
+				<p class="inline">{$customer.email}</p>
+			</div>
+
+			<div class="divider divider-horizontal"></div>
+
+			{#each Object.entries($customer.formData || {}) as [key, value]}
+				<div>
+					<div class="font-semibold capitalize">{key}</div>
+					<p class="inline">{value}</p>
+				</div>
+			{/each}
+
+			<div class="card-actions ml-auto justify-end">
+				<button class="btn btn-secondary">Edit</button>
+			</div>
+		</div>
+	</div>
+</div>
--- apps/booking/src/routes/(main)/checkout/payment/+page.svelte
+++ apps/booking/src/routes/(main)/checkout/payment/+page.svelte
@@ -2,6 +2,7 @@
 	import { Elements, PaymentElement } from 'svelte-stripe';
 	import checkoutStore from '$lib/stores/checkout';
 	import Loading from '$lib/components/Loading.svelte';
+	import CustomerSummary from '$lib/sections/checkout/payment/CustomerSummary.svelte';
 
 	const {
 		paymentStore: { clientSecret, stripe, onSubmit, elements, loading }
@@ -13,22 +14,26 @@
 	<p>Your card details are safe and secure with us. We use Stripe to process payments.</p>
 </div>
 
-<div class="card max-w-xl bg-base-200">
-	{#if $loading}
-		<Loading />
-	{:else if $stripe && $clientSecret}
-		<div class="card-body">
-			<Elements stripe={$stripe}>
-				<form on:submit|preventDefault={onSubmit}>
-					<Elements stripe={$stripe} clientSecret={$clientSecret} bind:elements={$elements}>
-						<PaymentElement />
-					</Elements>
+<div class="flex flex-col gap-4">
+	<CustomerSummary />
 
-					<div class="card-actions mt-4 justify-end">
-						<button class="btn btn-primary">Pay</button>
-					</div>
-				</form>
-			</Elements>
-		</div>
-	{/if}
+	<div class="card max-w-xl bg-base-200">
+		{#if $loading}
+			<Loading />
+		{:else if $stripe && $clientSecret}
+			<div class="card-body">
+				<Elements stripe={$stripe}>
+					<form on:submit|preventDefault={onSubmit}>
+						<Elements stripe={$stripe} clientSecret={$clientSecret} bind:elements={$elements}>
+							<PaymentElement />
+						</Elements>
+
+						<div class="card-actions mt-4 justify-end">
+							<button class="btn btn-primary">Pay</button>
+						</div>
+					</form>
+				</Elements>
+			</div>
+		{/if}
+	</div>
 </div>
