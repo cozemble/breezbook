d759581061ee8b1a7a56731aa46c151ef134e72f
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-02-22 17:41:47 +0300

    feat(booking): error page go back action, better design

--- apps/booking/src/routes/+error.svelte
+++ apps/booking/src/routes/+error.svelte
@@ -9,31 +9,43 @@
 		console.error('Error loading page', $page.status, $page.error?.message);
 		console.log('Error page', $page);
 	});
+
+	const goBack = () => {
+		history.back();
+	};
 </script>
 
 <svelte:head>
 	<title>Error | Breezbook</title>
 </svelte:head>
 
-<main class="min-h-screen flex flex-col justify-center items-center bg-base-100">
-	<a class="p-10" href={utils.link.removeSubdomain($page.url)}>
-		<BreezbookLogo />
-	</a>
+<div class="p-10">
+	<header class="">
+		<a href={utils.link.removeSubdomain($page.url)}>
+			<BreezbookLogo />
+		</a>
+	</header>
 
-	<section class="hero flex-grow items-center justify-center text-center">
-		<div class="flex flex-col justify-center items-center">
-			<h1
-				class="text-9xl font-bold mb-7
+	<main class="min-h-screen flex flex-col justify-center items-center bg-base-100 pb-[10vh]">
+		<section class="hero flex-grow items-center justify-center text-center">
+			<div class="flex flex-col justify-center items-center">
+				<h1
+					class="text-[20vh] font-bold mb-[0.4em] leading-none
 					bg-gradient-to-tr from-primary via-secondary to-accent
 					bg-clip-text text-transparent
 					"
-			>
-				{$page.status}
-			</h1>
+				>
+					{$page.status}
+				</h1>
 
-			<h2 class="text-secondary-content text-6xl font-semibold mb-24">
-				{$page?.error?.message || 'Something went wrong'}
-			</h2>
-		</div>
-	</section>
-</main>
+				<h2 class="text-secondary-content text-3xl sm:text-5xl font-semibold mb-6">
+					{$page?.error?.message || 'Something went wrong'}
+				</h2>
+
+				<p class="opacity-60">We're sorry, but something went wrong. Please try again later.</p>
+
+				<button class="btn btn-primary btn-lg mt-16" on:click={goBack}> Go back </button>
+			</div>
+		</section>
+	</main>
+</div>
