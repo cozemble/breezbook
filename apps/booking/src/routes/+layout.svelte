23291d0247ece12487270848d13576d42d4fdcf7
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-03-18 18:39:14 +0300

    feat(booking): import stripe on every page

--- apps/booking/src/routes/+layout.svelte
+++ apps/booking/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
-	import '$lib/styles/main.css';
+	import '$lib/styles/main.css'; // Global styles, needed for TailwindCSS and DaisyUI
+	import '@stripe/stripe-js'; // For advanced fraud detection and PCI compliance: https://docs.stripe.com/js/including
 </script>
 
 <slot />
