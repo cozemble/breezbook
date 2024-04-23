1b52d1363c0b9fa477a87611681e19918403411d
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-04-23 16:54:51 +0300

    fix(booking): app breaks when theme is {}

--- apps/booking/src/routes/(main)/+layout.svelte
+++ apps/booking/src/routes/(main)/+layout.svelte
@@ -21,7 +21,7 @@
 <div
 	class="min-h-screen flex flex-col items-center"
 	data-theme={tenant.theme?.theme}
-	style={tenant.theme?.font.sans && `font-family: ${tenant.theme?.font.sans};`}
+	style={tenant.theme?.font?.sans && `font-family: ${tenant.theme?.font?.sans};`}
 >
 	<Header />
 	<main class="p-3 md:p-6 flex-grow w-full flex flex-col items-center">
