5d2c80ad2d56b9b5479a3d417fd1cfe52489a6c0
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-05-04 19:06:06 +0300

    chore(booking): listTenants route only accessible on dev mode

--- apps/booking/src/routes/listTenants/+page.server.ts
+++ apps/booking/src/routes/listTenants/+page.server.ts
@@ -1,8 +1,11 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import * as utils from '$lib/common/utils';
+import config from '$lib/common/config';
 
 export const load: PageServerLoad = async ({ url }) => {
+	if (!config.devMode) error(403, 'This page is not accessible in production mode');
+
 	const subdomain = utils.link.getSubdomain(url);
 
 	const isSubdomainWww = subdomain === 'www';
