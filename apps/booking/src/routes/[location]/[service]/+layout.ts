69e61fdee17d37cb4ac5ab89ea9f62aabf01bc7f
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-05-12 20:23:59 +0300

    fix(booking): service page load error from layout issue

--- apps/booking/src/routes/[location]/[service]/+layout.ts
+++ apps/booking/src/routes/[location]/[service]/+layout.ts
@@ -1,8 +1,8 @@
 import { error } from '@sveltejs/kit';
-import type { LayoutServerLoad } from './$types';
+import type { LayoutLoad } from './$types';
 import api from '$lib/common/api';
 
-export const load: LayoutServerLoad = async ({ params, parent }) => {
+export const load: LayoutLoad = async ({ params, parent }) => {
 	const tenantSlug = await parent().then((p) => p.tenant.slug);
 	const service = await api.service.getOne(tenantSlug, params.service);
 
