64dfad2779eb75cbb2550f88106b49d18c2c99e3
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-05-12 20:01:12 +0300

    refactor(booking): remove listTenants route
    to prevent confusion for users

--- apps/booking/src/routes/(main)/+page.svelte
+++
@@ -1,3 +0,0 @@
-<!--  @component
-  main page of tenant. Not actually used but needed for routing.
- -->
--- apps/booking/src/routes/+layout.ts
+++ apps/booking/src/routes/+layout.ts
@@ -2,7 +2,11 @@ import posthog from 'posthog-js';
 import { browser } from '$app/environment';
 import type { LayoutLoad } from './$types';
 
-export const load: LayoutLoad = async () => {
+import api from '$lib/common/api';
+import * as utils from '$lib/common/utils';
+import { error, redirect } from '@sveltejs/kit';
+
+export const load: LayoutLoad = async ({ params, url }) => {
 	if (browser) {
 		posthog.init('phc_asps2z1RUkEjKSSWV1ezUHyt2epCGGqDhLTejoYA2a7', {
 			api_host: 'https://us.i.posthog.com'
@@ -11,5 +15,26 @@ export const load: LayoutLoad = async () => {
 
 		Object.assign(window, { posthog });
 	}
-	return;
+
+	const subdomain = utils.link.getSubdomain(url);
+	const isSubdomainWww = subdomain === 'www';
+	if (!subdomain || isSubdomainWww) redirect(307, 'https://breezbook.com'); // redirect to list of tenants for demo purposes}
+
+	const tenant = await api.tenant.getOne(subdomain);
+	if (!tenant) error(404, 'Tenant not found');
+
+	const locationSlug = params.location;
+	// default to first location if no location is provided
+	if (!locationSlug) {
+		console.log('redirecting to first location');
+		redirect(307, `/${tenant.locations[0].slug}`);
+	}
+
+	const location = tenant.locations.find((location) => location.slug === locationSlug);
+	if (!location) error(404, 'Location not found');
+
+	return {
+		tenant,
+		location
+	};
 };
