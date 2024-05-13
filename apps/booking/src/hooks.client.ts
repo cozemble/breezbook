0ce425d7177e5ba8385ef2d66a7c043dc1996d38
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-05-13 15:49:31 +0300

    chore(booking): disable highlight on local dev server

--- apps/booking/src/hooks.client.ts
+++ apps/booking/src/hooks.client.ts
@@ -1,18 +1,21 @@
+import { dev } from '$app/environment';
 import config from '$lib/common/config';
 import { H } from 'highlight.run';
 
-H.init('1epk540g', {
-	environment: config.devMode ? 'development' : 'production',
-	version: 'commit:abcdefg12345',
-	tracingOrigins: true,
-	networkRecording: {
-		enabled: true,
-		recordHeadersAndBody: true,
-		urlBlocklist: [
-			// insert full or partial urls that you don't want to record here
-			// Out of the box, Highlight will not record these URLs (they can be safely removed):
-			'https://www.googleapis.com/identitytoolkit',
-			'https://securetoken.googleapis.com'
-		]
-	}
-});
+if (!dev) {
+	H.init('1epk540g', {
+		environment: config.devMode ? 'development' : 'production',
+		version: 'commit:abcdefg12345',
+		tracingOrigins: true,
+		networkRecording: {
+			enabled: true,
+			recordHeadersAndBody: true,
+			urlBlocklist: [
+				// insert full or partial urls that you don't want to record here
+				// Out of the box, Highlight will not record these URLs (they can be safely removed):
+				'https://www.googleapis.com/identitytoolkit',
+				'https://securetoken.googleapis.com'
+			]
+		}
+	});
+}
