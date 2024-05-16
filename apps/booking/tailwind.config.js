a6f6ce1171dffa62558c5b3c178cbcf01ceec93b
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-05-16 16:32:03 +0300

    feat(booking): add breezbook theme

--- apps/booking/tailwind.config.js
+++ apps/booking/tailwind.config.js
@@ -19,6 +19,39 @@ export default {
 	},
 	plugins: [daisyui],
 	daisyui: {
-		themes: ['winter', 'corporate', 'dim', 'sunset', 'cyberpunk', 'lemonade']
+		themes: [
+			{
+				breezbook: {
+					primary: '#347fff',
+					'primary-content': '#ffffff',
+					secondary: '#58cdfa',
+					'secondary-content': '#051016',
+					accent: '#5fd08e',
+					'accent-content': '#031007',
+					neutral: '#070e20',
+					'neutral-content': '#c6c8ce',
+
+					'base-100': '#fafeff',
+					'base-200': '#e7edf2',
+					'base-300': '#d0dde7',
+					'base-content': '#151616',
+
+					info: '#7dd3fc',
+					'info-content': '#051016',
+					success: '#4ade66',
+					'success-content': '#021203',
+					warning: '#facc15',
+					'warning-content': '#150f00',
+					error: '#f87171',
+					'error-content': '#150404'
+				}
+			},
+			'corporate',
+			'winter',
+			'dim',
+			'sunset',
+			'cyberpunk',
+			'lemonade'
+		]
 	}
 };
