8deb016a7a5bb441c5e784075df8206b6681968e
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-05-16 17:14:21 +0300

    feat(booking): add breezbook logo to footer

--- apps/booking/src/lib/sections/home/Footer.svelte
+++ apps/booking/src/lib/sections/home/Footer.svelte
@@ -2,6 +2,7 @@
 	import { page } from '$app/stores';
 	import tenantStore from '$lib/stores/tenant';
 	import routeStore from '$lib/stores/routes';
+	import Logo from '$lib/assets/breezbook-logo.svg';
 
 	const routes = routeStore.get();
 	const tenant = tenantStore.get();
@@ -10,9 +11,11 @@
 <footer class="footer px-3 md:px-6 py-4 flex justify-center bg-neutral text-neutral-content">
 	<div class="w-full max-w-7xl flex justify-between">
 		<p class="text-center">{tenant.name}</p>
-		<p class="text-center text-xs opacity-50 inline">
+		<p class="text-center text-xs inline">
 			Provided by
-			<a href={routes.breezbook()} class="link-hover"> breezbook </a>
+			<a href={routes.breezbook()} class="hover:opacity-70 rounded-sm">
+				<img src={Logo} alt="Breezbook" class="h-4 w-auto inline mb-1" />
+			</a>
 			&copy; {new Date().getFullYear()}
 		</p>
 	</div>
