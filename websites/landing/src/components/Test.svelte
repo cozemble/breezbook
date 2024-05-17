17b9ab8ebfc68e941f14c931c91cc2b785442b07
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-05-17 19:37:38 +0300

    chore(landing): add svelte

---
+++ websites/landing/src/components/Test.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	let test = 'Hello World';
+
+	const onClick = () => {
+		test = 'Hello Mars!';
+	};
+</script>
+
+<div class="card bg-base-200">
+	<div class="card-body bg-base-300">
+		<h1>{test}</h1>
+		<div class="card-actions">
+			<button class="btn" on:click={onClick}>Button</button>
+		</div>
+	</div>
+</div>
