25baa426da1a05af063577f34053ff9649b5be3a
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-05-25 19:41:51 +0300

    feat(landing): header scrolled state styling
    required migrating it to svelte

---
+++ websites/landing/src/components/Header.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import HeaderNav from './HeaderNav.svelte';
+
+	let scrollY = 0;
+
+	$: isScrolled = scrollY > 30;
+
+	$: console.log(isScrolled);
+</script>
+
+<svelte:window bind:scrollY />
+
+<!-- <header class="navbar shadow-sm justify-center"> -->
+<header
+	class="navbar px-[var(--page-padding)] justify-between
+	bg-base-100
+				sticky top-0 left-0 z-50
+				{isScrolled ? 'shadow-xl' : 'shadow-sm'}
+				"
+>
+	<a href="/">
+		<img src="./breezbook-logo.svg" alt="Breezbook Logo" class="h-7 sm:h-8 md:h-10" />
+	</a>
+
+	<HeaderNav />
+</header>
---
+++ websites/landing/src/components/HeaderNav.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	const links: {
+		text: string;
+		href: string;
+	}[] = [
+		{ text: 'Features', href: '#features' },
+		{ text: 'Pricing', href: '/coming-soon' },
+		{ text: 'Docs', href: '/coming-soon' },
+		{ text: 'Contact', href: '/coming-soon' }
+	];
+</script>
+
+<nav class="hidden sm:block">
+	<ul class="flex">
+		{#each links as link}
+			<li>
+				<a href={link.href} class="btn btn-ghost">
+					{link.text}
+				</a>
+			</li>
+		{/each}
+	</ul>
+</nav>
+
+<div class="drawer drawer-end sm:hidden w-20 p-0 flex justify-end">
+	<input id="my-drawer" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content">
+		<label for="my-drawer" class="drawer-button btn btn-ghost btn-circle">
+			<Icon icon="solar:hamburger-menu-broken" class="text-4xl text-neutral" />
+		</label>
+	</div>
+
+	<div class="drawer-side">
+		<label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"> </label>
+		<ul class="menu p-4 w-40 min-h-full bg-base-200 text-base-content">
+			{#each links as link}
+				<li>
+					<a href={link.href} class="link link-hover">
+						{link.text}
+					</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
+</div>
