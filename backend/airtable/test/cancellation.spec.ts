0c393ba3f3f54ca0c739f9a2422485ba5525c7ac
mikehogan <me@mikehogan.net>
2024-06-17 12:21:48 +0100

    small tidy

--- backend/airtable/test/cancellation.spec.ts
+++ backend/airtable/test/cancellation.spec.ts
@@ -2,7 +2,7 @@ import {IsoDate, isoDate, isoDateFns, SystemClock} from '@breezbook/packages-cor
 import {describe, expect, test} from 'vitest';
 import {CancellationGranted} from '@breezbook/backend-api-types';
 import {doCancellationRequest, doCommitCancellation} from '../src/express/cancellation.js';
-import {DbBooking, DbCancellationGrant, DbService} from '../src/prisma/dbtypes.js';
+import {DbCancellationGrant, DbService} from '../src/prisma/dbtypes.js';
 import {updateBooking, updateCancellationGrant} from '../src/prisma/breezPrismaMutations.js';
 import {jsDateFns} from '@breezbook/packages-core/dist/jsDateFns.js';
 import {HttpError} from '../src/infra/functionalExpress.js';
@@ -11,20 +11,20 @@ import {DbBookingAndResourceRequirements} from "../src/express/getEverythingForA
 
 test("can't get a cancellation grant for a booking in the past", () => {
     const theBooking = makeDbBooking(isoDateFns.addDays(isoDate(), -1));
-    const outcome = doCancellationRequest([], [], [], [makeDbService()], theBooking, [],[],new SystemClock()) as HttpError;
+    const outcome = doCancellationRequest([], [], [], [makeDbService()], theBooking, [], [], new SystemClock()) as HttpError;
     expect(outcome._type).toBe('http.error');
     expect(outcome.status).toBe(400);
 });
 
 test('full refund if there are no refund rules, and booking is in the future', () => {
     const theBooking = makeDbBooking(isoDateFns.addDays(isoDate(), 1));
-    const outcome = doCancellationRequest([], [], [],[makeDbService()],theBooking, [],[],new SystemClock()) as CancellationGranted;
+    const outcome = doCancellationRequest([], [], [], [makeDbService()], theBooking, [], [], new SystemClock()) as CancellationGranted;
     expect(outcome._type).toBe('cancellation.granted');
     expect(outcome.refundPercentageAsRatio).toBe(1.0);
 });
 
-function makeDbBooking(yesterday: IsoDate) {
-    const theBooking: DbBookingAndResourceRequirements = {
+function makeDbBooking(yesterday: IsoDate): DbBookingAndResourceRequirements {
+    return {
         id: 'booking-id',
         environment_id: 'environment-id',
         tenant_id: 'tenant-id',
@@ -33,17 +33,15 @@ function makeDbBooking(yesterday: IsoDate) {
         date: yesterday.value,
         status: 'confirmed',
         service_id: 'service-id',
-        time_slot_id: null,
         start_time_24hr: '09:00',
         end_time_24hr: '10:00',
         customer_id: 'customer-id',
         order_id: 'order-id',
         booking_resource_requirements: [],
     };
-    return theBooking;
 }
 
-function makeDbService():DbService {
+function makeDbService(): DbService {
     return {
         id: 'service-id',
         tenant_id: 'tenant-id',
