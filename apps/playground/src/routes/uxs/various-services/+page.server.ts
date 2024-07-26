8e82a370328ab2da4d57639997cfac725163ee4c
mikehogan <me@mikehogan.net>
2024-07-26 15:35:03 +0100

    fully qualified preview url

--- apps/playground/src/routes/uxs/various-services/+page.server.ts
+++ apps/playground/src/routes/uxs/various-services/+page.server.ts
@@ -1,10 +1,10 @@
-import type { PageServerLoad } from './$types';
+import type {PageServerLoad} from './$types';
 
 export const load: PageServerLoad = async () => {
     return {
         title: "Demo of Breezbook Scheduling Rules",
         description: "A demo that proves different kinds of service definition of bookings and appointments",
-        imageUrl: "/images/breezbook-scheduling-options-social-share.jpg",
+        imageUrl: "https://breezbook-playground.vercel.app/images/breezbook-scheduling-options-social-share.jpg",
         url: "https://breezbook-playground.vercel.app/uxs/various-services",
     };
 };
