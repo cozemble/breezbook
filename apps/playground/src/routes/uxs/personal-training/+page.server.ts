e2832f4e83f17fbeb703a206a9e9decabff4e25c
mikehogan <me@mikehogan.net>
2024-08-01 19:17:06 +0100

    personal training demo: social share stuff

--- apps/playground/src/routes/uxs/personal-training/+page.server.ts
+++ apps/playground/src/routes/uxs/personal-training/+page.server.ts
@@ -4,7 +4,7 @@ export const load: PageServerLoad = async () => {
     return {
         title: "Demo of personal training booking using Breezbook",
         description: "This is a demo app that shows the capabilities of Breezbook, by allowing you to book a personal training session.",
-        imageUrl: "https://breezbook-playground.vercel.app/images/breezbook-scheduling-options-social-share.jpg",
+        imageUrl: "https://breezbook-playground.vercel.app/images/breez-gym-social-share.jpg",
         url: "https://breezbook-playground.vercel.app/uxs/personal-training",
     };
 };
